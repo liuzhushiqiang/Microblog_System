c938aeb71176864ba2234370bf2699a31c87b4b2
liuzhushiqiang <liuzhushiqiang@sina.com>
2014-09-22 22:09:40 +0800

    continue the login module

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -9,38 +9,6 @@ class IndexController extends BaseController{
 
     public function indexAction()
     {
-    	if (isset($_Session['user_id'])) {
-            $this->render("index");
-        } else {
-            $this->_forward("login", "login");
-        }
-    }
-    
-    public function autorefreshAction(){
-    	$mb_post = new Mb_Post();
-    	
-    	$this->view->res = $mb_post->get_weibo(10);
-    	
-    	$this->render('autorefresh');
-    }
-    
-    public function commentAction(){
-    	$mb_comment = new Mb_Comment();
-    	$this->view->res = $mb_comment->get_comment(1);
-    	$this->render('comment');
-    }
-    
-    public function jiaguanzhuAction(){
-    	$mb_post = new Mb_Post();
-    	$mb_post->jiaguanzhu(1);
-    	$this->render('comment');
-    }
-    
-    public function sousuoAction(){
-    	$mb_post = new Mb_Post();
-    	$key_word = $_GET['key_word'];
-    	$this->view->res = $mb_post->sousuo($key_word);
-    	$this->render('sousuo');
+    	$this->_forward("index", "Weibo");
     }
 }
-
