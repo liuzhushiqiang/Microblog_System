e43da264f36d5228abbb897f357ac03d2473b982
liuzhushiqiang <liuzhushiqiang@sina.com>
2014-09-23 20:36:57 +0800

    finish the login module

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -9,6 +9,42 @@ class IndexController extends BaseController{
 
     public function indexAction()
     {
-    	$this->_forward("index", "Weibo");
+    	session_start();
+        if (isset($_SESSION['user_id'])) {
+            $mb_post = new Mb_Post();
+        
+            $this->view->res = $mb_post->get_weibo(0);
+        
+            $this->render("index");
+        } else {
+            $this->render("login");
+        }
+    }
+
+    public function autorefreshAction(){
+        $mb_post = new Mb_Post();
+        
+        $this->view->res = $mb_post->get_weibo(10);
+        
+        $this->render('autorefresh');
+    }
+    
+    public function commentAction(){
+        $mb_comment = new Mb_Comment();
+        $this->view->res = $mb_comment->get_comment(1);
+        $this->render('comment');
+    }
+    
+    public function jiaguanzhuAction(){
+        $mb_post = new Mb_Post();
+        $mb_post->jiaguanzhu(1);
+        $this->render('comment');
+    }
+    
+    public function sousuoAction(){
+        $mb_post = new Mb_Post();
+        $key_word = $_GET['key_word'];
+        $this->view->res = $mb_post->sousuo($key_word);
+        $this->render('sousuo');
     }
 }
--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -3,40 +3,35 @@
 require_once APPLICATION_PATH.'/models/Mb_User.php';
 require_once 'BaseController.php';
 
-
+session_start();
 
 class  LoginController extends BaseController{
-	public function loginAction(){
-		session_start();
+	/**
+	 * [loginAction description]
+	 * @return [type] [description]
+	 */
+	public function loginAction() {		
+		$u_email = $_REQUEST['email'];
+		$u_pw = $_REQUEST['password'];
 
-        if (isset($_SESSION['nick_name'])) {
-        	//already login
-            $mb_post = new Mb_Post();
-        
-            $this->view->res = $mb_post->get_weibo(0);
-            $this->view->friends = $mb_post->get_friends(0);
-        
-            $this->_forward("index", "index");
-        } else {
-        	//need login
-            $this->view->login_info = "Please log in!";
-            $this->render("login");
-        }
-
-
-	// 	$u_email = $_Post['email'];
-	// 	$u_pw = $_Post['password'];
-	// 	$mb_user = new Mb_User();	
-	// 	if(($res = $mb_user->login_check($u_email, $u_pw)) != NULL){			
-	// 	//file_put_contents(APPLICATION_PATH."/logfile.txt", "OK?", FILE_APPEND);
-	// 		$nickname = $res[0]['nickname'];
-	// 		//$_SESSION['nickname'] = $res[0]['nickname'];
-	// 		//$_SESSION['uid'] = $res[0]['id'];
-	// 		$this->_redirect("index?nickname=$nickname");
-	// 	}else{
-	// 		$this->view->login_error = 'Wrong Password';
-	// 		$this->render("login/login");
-	// 	}
+		$mb_user = new Mb_User();	
+		if (($res = $mb_user->login_check($u_email, $u_pw)) != NULL) {
+			$_SESSION['nick_name'] = $res[0]['nickname'];
+			$_SESSION['user_id'] = $res[0]['id'];
+			$this->_redirect("");
+		} else {
+			$this->view->login_info = "Wrong Passwd";
+			$this->_forward("index", "index");
 		}
+	}
+
+	/**
+	 * [logoutAction description]
+	 * @return [type] [description]
+	 */
+	public function logoutAction(){
+		session_destroy();
+		$this->_forward("index", "index");
+	}
 }
 ?>
