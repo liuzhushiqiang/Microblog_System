9da237449a92fa4cc0834973469bfdabf12ba219
liuzhushiqiang <liuzhushiqiang10@gmail.com>
2015-05-06 14:04:09 +0800

    test

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -14,20 +14,5 @@ class TestController extends Zend_Controller_Action
 			));
 		$this->view->res = $redis->get('name');
 	}
-
-	public function test2Action()
-	{
-		$http = new swoole_http_server("0.0.0.0", 9501);
-
-$http->on('request', function ($request, $response) {
-    $response->gzip();
-    $response->header("Content-Type", "text/html; charset=utf-8");
-    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
-});
-	}
-
-	public function test3Action()
-	{
-	}
 }
 ?>
